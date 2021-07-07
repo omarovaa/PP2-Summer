8dbf39fa77ba220c3c93b32ce335b0c076267c8e
omarovaa <51435221+omarovaa@users.noreply.github.com>
2021-07-08 01:44:00 +0600

    Delete TSIS4 directory

--- TSIS4/1.py
+++
@@ -1,3 +0,0 @@
-import re
-for i in range(int(input())):
-    print(bool(re.match(r"^[+-]?[0-9]*\.[0-9]+&", input())))
--- TSIS4/5.py
+++
@@ -1,3 +0,0 @@
-import re
-for i in range(int(input())):
-    print('YES' if re.search(r"^[789]\d{9}$", input()) else 'NO')
